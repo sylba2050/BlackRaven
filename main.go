2076ee9246e7d4373927f322446985722d40db3c
Masataka Hisasue <masataka.hisasue@optim.co.jp>
2019-03-07 16:34:09 +0900

    feat: sub db

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
     e.Use(middleware.Logger())
     e.Use(middleware.CORS())
 
-    db, err := gorm.Open("sqlite3", "test.sqlite3")
+    db, err := gorm.Open("sqlite3", "DB/main.sqlite3")
     if err != nil {
         panic("failed to connect database")
     }
@@ -27,10 +27,19 @@ func main() {
     db.AutoMigrate(&MsTNDB.Top{})
     db.AutoMigrate(&MsTNDB.Under{})
 
+    db_tmp, err := gorm.Open("sqlite3", "DB/temp.sqlite3")
+    if err != nil {
+        panic("failed to connect database")
+    }
+    defer db_tmp.Close()
+
+    db_tmp.AutoMigrate(&MsTNDB.Top{})
+    db_tmp.AutoMigrate(&MsTNDB.Under{})
+
     e.GET("api/default", generator.Default(db))
     e.GET("api/json", generator.JSON(db))
-    e.POST("api/create/top", generator.InsertTop(db))
-    e.POST("api/create/under", generator.InsertUnder(db))
+    e.POST("api/create/top", generator.InsertTop(db_tmp))
+    e.POST("api/create/under", generator.InsertUnder(db_tmp))
 
     e.Start(":1417")
 }
